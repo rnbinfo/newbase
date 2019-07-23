b2f8c811fe7ee656105b3ffadb50a5baa51e9dd3
wuji <wuji@local>
2019-07-23 16:34:21 +0800

    更新valid方式 拆分web controller及page功能

--- parent/web/src/main/java/com/rnbbusiness/newbase/web/api/HttpInnerRequest.java
+++ parent/web/src/main/java/com/rnbbusiness/newbase/web/api/HttpInnerRequest.java
@@ -2,11 +2,13 @@ package com.rnbbusiness.newbase.web.api;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+
 
 @Data
 public class HttpInnerRequest<T> implements HttpRequest {
 
     private HttpInnerRequestHeader header;
+    @Valid
     private T body;
-
 }
--- parent/web/src/main/java/com/rnbbusiness/newbase/web/api/HttpOuterRequest.java
+++ parent/web/src/main/java/com/rnbbusiness/newbase/web/api/HttpOuterRequest.java
@@ -2,11 +2,14 @@ package com.rnbbusiness.newbase.web.api;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+
 
 @Data
 public class HttpOuterRequest<T> implements HttpRequest {
 
     private HttpOuterRequestHeader header;
+    @Valid
     private T body;
 
 }
