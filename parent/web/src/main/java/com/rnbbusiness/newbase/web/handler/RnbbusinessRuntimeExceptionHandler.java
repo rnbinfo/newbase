38e9efd6ac7e2f7a50b02d9c0ed1fae11e3533e9
wuji <wuji@rnbbusiness.com>
2019-08-13 14:49:21 +0800

    增加错误信息国际化处理

--- parent/web/src/main/java/com/rnbbusiness/newbase/web/handler/RnbbusinessRuntimeExceptionHandler.java
+++ parent/web/src/main/java/com/rnbbusiness/newbase/web/handler/RnbbusinessRuntimeExceptionHandler.java
@@ -3,20 +3,35 @@ package com.rnbbusiness.newbase.web.handler;
 import com.rnbbusiness.newbase.exception.RnbbusinessRuntimeException;
 import com.rnbbusiness.newbase.web.api.HttpResponse;
 import com.rnbbusiness.newbase.web.api.HttpResponseHeader;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.Locale;
+
 @ControllerAdvice
 public class RnbbusinessRuntimeExceptionHandler {
+    @Resource
+    private MessageSource messageSource;
+
     @ExceptionHandler(value = RnbbusinessRuntimeException.class)
     @ResponseBody
-    public HttpResponse<String> defaultRestExceptionHandler(RnbbusinessRuntimeException e) {
+    public HttpResponse<String> defaultRestExceptionHandler(RnbbusinessRuntimeException e, Locale locale) {
+        // 国际化处理error message
+        String errorMessage;
+        try {
+            errorMessage = messageSource.getMessage(e.getErrorMessage(), null, locale);
+        } catch (Exception i18nException) {
+            i18nException.printStackTrace();
+            errorMessage = e.getErrorMessage();
+        }
         HttpResponse<String> response = new HttpResponse<>();
         HttpResponseHeader responseHeader = new HttpResponseHeader();
         responseHeader.setTranStatus("false");
         responseHeader.setErrorCode(e.getErrorCode());
-        responseHeader.setErrorMessage(e.getErrorMessage());
+        responseHeader.setErrorMessage(errorMessage);
         response.setHeader(responseHeader);
         e.printStackTrace();
         return response;
