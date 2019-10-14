a05446f240018d957d8f7ad615c70e54a786c4d6
wuji <wuji@rnbbusiness.com>
2019-10-14 16:59:35 +0800

    调整MD5算法方式

--- parent/toolkit/src/main/java/com/rnb/newbase/toolkit/security/MD5Sign.java
+++ parent/toolkit/src/main/java/com/rnb/newbase/toolkit/security/MD5Sign.java
@@ -1,70 +1,37 @@
 package com.rnb.newbase.toolkit.security;
 
 
-import java.security.MessageDigest;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.UnsupportedEncodingException;
 
 public class MD5Sign {
-    private static final String ALGORITHM = "MD5";
-
-    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5',
-            '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-
     /**
-     * encode string
-     *
-     * @param algorithm
-     * @param str
-     * @return String
+     * 将字符串MD5加码 生成32位md5码
      */
-    public static String encode(String algorithm, String str) {
-        if (str == null) {
-            return null;
-        }
+    public static String encodeAsMd5(String inStr,  String charset) {
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
-            messageDigest.update(str.getBytes());
-            return getFormattedText(messageDigest.digest());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            return DigestUtils.md5Hex(inStr.getBytes(charset));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Md5 signature failed");
         }
-
     }
 
     /**
-     * encode By MD5
-     *
-     * @param str
-     * @return String
+     * 验证签名
      */
-    public static String encodeByMD5(String str) {
-        if (str == null) {
-            return null;
+    public static boolean verify(String plainText, String sign, String charset) throws Exception {
+        if (sign == null || "".equals(sign.trim())) {
+            throw new Exception("Md5 signature is null or blank");
         }
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
-            messageDigest.update(str.getBytes());
-            return getFormattedText(messageDigest.digest());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        String mysign = encodeAsMd5(plainText, charset);
+        if (mysign.equals(sign)) {
+            return true;
+        } else {
+            return false;
         }
 
     }
 
-    /**
-     * Takes the raw bytes from the digest and formats them correct.
-     *
-     * @param bytes
-     *            the raw bytes from the digest.
-     * @return the formatted bytes.
-     */
-    private static String getFormattedText(byte[] bytes) {
-        int len = bytes.length;
-        StringBuilder buf = new StringBuilder(len * 2);
-        // 把密文转换成十六进制的字符串形式
-        for (int j = 0; j < len; j++) {
-            buf.append(HEX_DIGITS[(bytes[j] >> 4) & 0x0f]);
-            buf.append(HEX_DIGITS[bytes[j] & 0x0f]);
-        }
-        return buf.toString();
-    }
 }
