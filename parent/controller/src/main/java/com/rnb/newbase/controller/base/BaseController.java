2b8988073267a527ab461e5bd8beefe9bd19b943
wuji <wuji@rnbbusiness.com>
2020-04-10 13:38:59 +0800

    增加controller中获取用户ip方法 修正login时漏更新用户信息

--- parent/controller/src/main/java/com/rnb/newbase/controller/base/BaseController.java
+++ parent/controller/src/main/java/com/rnb/newbase/controller/base/BaseController.java
@@ -8,6 +8,7 @@ import com.rnb.newbase.exception.RnbRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public abstract class BaseController {
@@ -22,5 +23,33 @@ public abstract class BaseController {
         String conditionString = JSONObject.toJSONString(conditionObject);
         Map<String, Object> condition = JSONObject.parseObject(conditionString, new TypeReference<Map<String, Object>>(){});
         return condition;
+    }
+
+    /**
+     * 获取客户端
+     * @param request
+     * @return
+     */
+    public String getCliectIp(HttpServletRequest request)
+    {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        // 多个路由时，取第一个非unknown的ip
+        final String[] arr = ip.split(",");
+        for (final String str : arr) {
+            if (!"unknown".equalsIgnoreCase(str)) {
+                ip = str;
+                break;
+            }
+        }
+        return ip;
     }
 }
