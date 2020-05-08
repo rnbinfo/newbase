4187f6233c3feddc882423a7b5932bba6a0274d0
wuji <wuji@rnbinfo.com>
2020-05-08 16:50:38 +0800

    增加重定向swagger页面静态资源

--- parent/security/src/main/java/com/rnb/newbase/security/support/RnbSecurityWebMvcConfigurationSupport.java
+++ parent/security/src/main/java/com/rnb/newbase/security/support/RnbSecurityWebMvcConfigurationSupport.java
@@ -13,23 +13,22 @@ public abstract class RnbSecurityWebMvcConfigurationSupport extends WebMvcConfig
     private GetUserResolver getUserResolver;
 
     @Override
-    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         //注册@GetUser注解的实现类
         argumentResolvers.add(getUserResolver);
     }
 
     /**
      * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。 需要重新指定静态资源
+     *
      * @param registry
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(
-                "classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
-                "classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations(
-                "classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("docs.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
     }
 }
