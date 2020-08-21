257e5d92191f9e24c4ebe56a709738eba07dfdf4
wuji <wuji@rnbinfo.com>
2020-08-21 11:37:46 +0800

    增加内部服务调用请求

---
+++ parent/controller/src/main/java/com/rnb/newbase/controller/base/AbstractInnerRemoteService.java
@@ -0,0 +1,29 @@
+package com.rnb.newbase.controller.base;
+
+import com.alibaba.fastjson.JSON;
+import com.rnb.newbase.controller.api.HttpInnerRequest;
+import com.rnb.newbase.controller.api.HttpInnerRequestHeader;
+import com.rnb.newbase.toolkit.util.RandomUtil;
+
+public abstract class AbstractInnerRemoteService extends BaseRemoteService {
+    /**
+     * 生成内部服务请求
+     * @param requestBody
+     * @param requestUri
+     * @return
+     */
+    protected String generateInnerRequest(Object requestBody, String requestUri) {
+        HttpInnerRequestHeader header = new HttpInnerRequestHeader();
+        header.setMethod(requestUri);
+        header.setRequestSys("management");
+        header.setRequestNo(String.valueOf(System.currentTimeMillis()));
+        header.setNonce(RandomUtil.generateNoSymbleString(21));
+        header.setVersion("1.0");
+        header.setSign("aaa");
+        header.setLoginToken(RandomUtil.generateAllString(32));
+        HttpInnerRequest request = new HttpInnerRequest();
+        request.setHeader(header);
+        request.setBody(requestBody);
+        return JSON.toJSONString(request);
+    }
+}
