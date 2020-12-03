3d85898a424e4ce7c5055535a000cb5d9200fe52
wuji <wuji@rnbinfo.com>
2020-12-03 19:58:04 +0800

    更新至swagger3.0 更新mvcConfigurer

--- parent/swagger/src/main/java/com/rnb/newbase/swagger/SwaggerConfiguration.java
+++ parent/swagger/src/main/java/com/rnb/newbase/swagger/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.rnb.newbase.swagger.config;
+package com.rnb.newbase.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,27 +9,21 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfiguration {
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
+
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("《swagger.api接口》")//标题
-                .description("摘要")//描述
-                .termsOfServiceUrl("")//（不可见）条款地址，公司内部使用的话不需要配
-                .contact(new Contact("Devil", "https://host", "evil@henshinColor.com"))//作者信息
-                .version("1.0.0")//版本号
-                .build();
+        return new ApiInfoBuilder().title("盛硕科技").description("盛硕信息技术有限公司").contact(new Contact("开发者", "www.rnbinfo.com", "developer@rnbinfo.com"))
+                .version("1.0").build();
     }
 }
